a0a418cf7644d38c07d69a5eb160ec69e420f444
Reuben Sweetman-Gough <reuben.sweetmangough@dxc.com>
2023-11-03 08:50:13 +1300

    formatted 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -8,14 +8,14 @@ impl TopElf {
     fn get_top_elf(&mut self, curr_sum: u32) {
         if curr_sum > self.one {
             self.three = self.two;
-            self.two = self.one; 
+            self.two = self.one;
             self.one = curr_sum;
         } else if curr_sum > self.two {
-            self.three = self.two; 
+            self.three = self.two;
             self.two = curr_sum;
         } else if curr_sum > self.three {
             self.three = curr_sum;
-        }  
+        }
     }
 
     fn sum(&mut self) -> u32 {
@@ -24,21 +24,22 @@ impl TopElf {
 }
 
 fn parse_calc(input: &str) -> TopElf {
-
-    let mut top_elves: TopElf = TopElf { one: 0, two: 0, three: 0 };
+    let mut top_elves: TopElf = TopElf {
+        one: 0,
+        two: 0,
+        three: 0,
+    };
     let mut sum = 0;
 
     for line in input.lines() {
         let trimmed_line = line.trim();
 
         if trimmed_line.is_empty() {
-                top_elves.get_top_elf(sum);
-                sum = 0;
+            top_elves.get_top_elf(sum);
+            sum = 0;
         } else {
             match trimmed_line.parse::<u32>() {
-                Ok(num) => {
-                    sum += num
-                }
+                Ok(num) => sum += num,
                 Err(e) => println!("{e}"),
             }
         }
@@ -47,10 +48,9 @@ fn parse_calc(input: &str) -> TopElf {
     top_elves
 }
 
-
 pub fn part_one(input: &str) -> Option<u32> {
-   let top_elf = parse_calc(input);
-   println!("{}", top_elf.one);
+    let top_elf = parse_calc(input);
+    println!("{}", top_elf.one);
     Some(top_elf.one)
 }
 
